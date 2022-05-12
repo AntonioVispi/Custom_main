bd8a199857c48803acf6fdbc8fc1ec5512c246d5
AntonioVispi <102518682+AntonioVispi@users.noreply.github.com>
2022-05-12 12:43:53 +0200

    Update assemble_data.py

--- assemble_data.py
+++ assemble_data.py
@@ -1,7 +1,9 @@
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import misc
+#from scipy import misc   #levato
+import imageio  #aggiunto
+from PIL import Image  #aggiunto
 import os
 from tqdm import tqdm
 
@@ -23,14 +25,14 @@ for family in tqdm(file_name_arr):
 	# Create a zero filled base image
 	for i, file in enumerate(glob.glob(atri_dir+family+'*.png')):
 		# Read the image
-		read_image = misc.imread(file, flatten=True)
+		read_image = imageio.imread(file, as_grey=True)   #misc.imread(file, flatten=True)
 		border_color = read_image[0,0]
 		read_image[read_image == border_color] = 0
 		read_image[read_image > 0] = 255
 		read_image = np.int8(read_image/255)
 
 		if i == 0:
-			mask = misc.imread(mask_dir+family+'_segmentation.png', flatten=True)
+			mask = imageio.imread(mask_dir+family+'_segmentation.png', as_grey=True)   #misc.imread(mask_dir+family+'_segmentation.png', flatten=True)
 			base_image = np.ones(read_image.shape, dtype=int) # Healthy Skin is 1
 			border_mask_color = mask[0,0]
 			base_image[mask == border_mask_color] = 0
@@ -63,5 +65,6 @@ for family in tqdm(file_name_arr):
 				base_image[base_image > 7] = 7
 		else:
 			print('ERROR: Invalid File Found!!!!')
-	cazzoooooooooooooooooooooooo
-	misc.toimage(base_image, cmin=0, cmax=255).save(output_dir+family+'_semantic.png')
+	#misc.toimage(base_image, cmin=0, cmax=255).save(output_dir+family+'_semantic.png')
+	PIL_image = Image.fromarray(np.uint8(base_image))   #Aggiunto
+	PIL_image.save(output_dir+family+'_semantic.png')   #Aggiunto
